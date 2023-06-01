767247f8cdbfec006edc6dc1d973a4ba54c10631
Érik Eiji <eriknibe1@gmail.com>
2023-06-01 15:57:29 -0300

    feat: add delete contacts function

--- src/services/clients/deleteClient.service.ts
+++ src/services/clients/deleteClient.service.ts
@@ -1,14 +1,26 @@
 import { Repository } from "typeorm";
 import Client from "../../entities/client.entity";
 import { AppDataSource } from "../../data-source";
+import deleteContactService from "../contacts/deleteContact.service";
 
 const deleteClientService = async (id: string): Promise<void> => {
   const clientRepository: Repository<Client> =
     AppDataSource.getRepository(Client);
 
-  const foundClient: Client | null = await clientRepository.findOneBy({
-    id,
+  const foundClient: Client | null = await clientRepository.findOne({
+    relations: {
+      contacts: true,
+    },
+    where: {
+      id,
+    },
   });
+
+  if (foundClient) {
+    foundClient.contacts.forEach(async (contact) => {
+      await deleteContactService(contact.id);
+    });
+  }
 
   await clientRepository.remove(foundClient!);
 };
